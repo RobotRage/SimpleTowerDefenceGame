234b4dc73eb55cafcc51c7b35feffac0c6d30ea4
Dylan Robertson <31269647+RobotRage@users.noreply.github.com>
2019-07-10 15:30:51 +1000

    added tower placement

--- Assets/Scripts/TowerPlaceController.cs
+++ Assets/Scripts/TowerPlaceController.cs
@@ -7,15 +7,50 @@ public class TowerPlaceController : MonoBehaviour
     bool placed = false;
 
     //true if tower is over a valid tower placement zone
-    bool canBePlaced = false;
+    bool canBePlaced = true;
+
+    int NonvalidHitboxCount = 0;
 
     private Vector3 mousePos;
 
+    SpriteRenderer m_SpriteRenderer;
+    Color col = Color.red;
     // Start is called before the first frame update
     void Start()
     {
+        m_SpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "NonValidPlaceLocation")
+        {
+            NonvalidHitboxCount++;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "NonValidPlaceLocation")
+        {
+            NonvalidHitboxCount--;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(NonvalidHitboxCount == 0)
+        {
+            canBePlaced = true;
+            m_SpriteRenderer.color = Color.white;
+        }
+        else
+        {
+            canBePlaced = false;
+            m_SpriteRenderer.color = col;
+        }
+
         //changed the tower's tag based on if its placed or unplaced
-        if(placed)
+        if (placed)
         {
             gameObject.tag = "Placed";
         }
@@ -23,17 +58,21 @@ public class TowerPlaceController : MonoBehaviour
         {
             gameObject.tag = "Unplaced";
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(!placed)
+        if (!placed)
         {
             //gets the mouse position
             mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             //sets the tower position to the mouse x and y position
             gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+
+            if(canBePlaced)
+            {
+                if(Input.GetMouseButtonDown(0))
+                {
+                    placed = true;
+                }
+            }
         }
     }
 }
