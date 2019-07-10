85d76ea345a1c50164fa40851d3c6decc1c83a54
Colby <31269003+Colbyjdx@users.noreply.github.com>
2019-07-11 00:59:26 +1000

    Pathing works! I'll add documentation later

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+
+    // Colby's code
+
+    public List<GameObject> targets = new List<GameObject>();
+    public int targetIndex = 0;
+    public int speed = 1;
+    public bool done = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool searching = true;
+        int index = 0;
+        while (searching)
+        {
+            GameObject node = GameObject.Find("node" + index);
+            if (node != null)
+            {
+                targets.Add(node);
+                print("found one");
+                index += 1;
+            }
+            else
+            {
+                searching = false;
+                print("found none");
+            }
+        }
+
+        print("finished searching, found " + targets.Count);
+
+        if (targets.Count == 0)
+        {
+            done = true;
+            print("done!");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!done)
+        {
+            if (transform.position != targets[targetIndex].transform.position)
+            {
+                Vector3 moveDir = (targets[targetIndex].transform.position - transform.position).normalized;
+                transform.position += moveDir * speed * Time.deltaTime;
+            }
+            else
+            {
+                targetIndex += 1;
+                if (targetIndex + 1 > targets.Count)
+                {
+                    done = true;
+                }
+            }
+        }
+
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemy, transform.position, transform.rotation);
+        StartCoroutine(Wait(15f));
+    }
+
+    IEnumerator Wait(float x)
+    {
+        yield return new WaitForSeconds(x);
+        Spawn();
+    }
+}
