0870e98f38d3c1256e7e3c0f2f1f1b82f5608b3d
Dylan Robertson <31269647+RobotRage@users.noreply.github.com>
2019-07-10 14:35:12 +1000

    added not fininshed simple tower selection

---
+++ Assets/Scripts/TowerCreation.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class TowerCreation : MonoBehaviour
+{
+    //an array of gameobjects which holds all the towers
+    //new towers can be added in unity inspector on the "TowerSpawn" object
+    public GameObject[] Towers;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //function is called when any button is pressed (must be assigned when new button is created)
+    public void CreateTower()
+    {
+        //array of towers that are not placed
+        GameObject[] NotPlacedTowers;
+
+        NotPlacedTowers = GameObject.FindGameObjectsWithTag("Unplaced");
+
+        //if there is an unplaced tower
+        if(NotPlacedTowers.Length > 0)
+        {
+            //delete all unplaced towers
+            foreach (GameObject tower in NotPlacedTowers)
+            {
+                Destroy(tower);
+            }
+        }
+        //if there are no unplaced towers
+        else
+        {
+            //prints the current selected button
+            string btnPressed = EventSystem.current.currentSelectedGameObject.name;
+            print(btnPressed);
+
+            //check which button was pressed and spawns the appropriate tower
+            if (btnPressed == "btnCreateTower_1")
+            {
+                Instantiate(Towers[0], new Vector3(0, 0, 0), Quaternion.identity);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/TowerPlaceController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerPlaceController : MonoBehaviour
+{
+    bool placed = false;
+
+    //true if tower is over a valid tower placement zone
+    bool canBePlaced = false;
+
+    private Vector3 mousePos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //changed the tower's tag based on if its placed or unplaced
+        if(placed)
+        {
+            gameObject.tag = "Placed";
+        }
+        else
+        {
+            gameObject.tag = "Unplaced";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!placed)
+        {
+            //gets the mouse position
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //sets the tower position to the mouse x and y position
+            gameObject.transform.position = new Vector3(mousePos.x, mousePos.y, 0);
+        }
+    }
+}
