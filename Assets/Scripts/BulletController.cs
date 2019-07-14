457e973e9ea651868c6e6a9b41fba298171db9c1
Dylan Robertson <31269647+RobotRage@users.noreply.github.com>
2019-07-14 19:48:24 +1000

    added some animations

--- Assets/Scripts/BulletController.cs
+++ Assets/Scripts/BulletController.cs
@@ -7,15 +7,31 @@ public class BulletController : MonoBehaviour
     public float bulletDamage;
     public Vector3 Direction;
     float thrust = 9f;
-
+    SpriteRenderer sprender;
     // Start is called before the first frame update
     void Start()
     {
+        sprender = gameObject.GetComponent<SpriteRenderer>();
+
+        if (Direction == new Vector3(0, 1, 0))
+        {
+            sprender.flipY = true;
+        }
+        else if (Direction == new Vector3(1, 0, 0))
+        {
+            transform.eulerAngles = new Vector3(0, 0, -90);
+            Direction = new Vector3(0, -1, 0);
+        }
+        else if (Direction == new Vector3(-1, 0, 0))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 90);
+            Direction = new Vector3(0, -1, 0);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        transform.Translate(Direction * thrust * Time.deltaTime);
+       transform.Translate(Direction * thrust * Time.deltaTime);
     }
 }
